71c7ea7bfe69b168d186ba457ba41719eddedd22
Ronit Kumar <ronit.kumar@acefone.com>
2024-05-14 15:21:43 +0530

    cors added

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    rawBody: true,
+    bodyParser: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
